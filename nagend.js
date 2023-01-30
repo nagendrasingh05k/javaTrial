2e429d4a578b074da641e565431a0378e57c7161
nagendrasingh05k <nagendra9395a@gmail.com>
2023-01-30 12:15:58 +0530

    first commit

---
+++ nagend.js
@@ -0,0 +1,84 @@
+console.log("this is my first javascript page");
+
+// let a = 67
+// console.log(a)
+// a = "nagendra"
+// console.log(a)
+
+// nn bb ss u - primitive in js
+
+// let a = null;
+// let b = 354;
+// let c = true;
+// let d = BigInt('576')
+// let e = "nagend"
+// let f = Symbol('I am nice symbol')
+// let g = undefined
+
+// console.log(g)
+// console.log(typeof g)
+
+// Object in js
+// const item = {
+//     "harry": true,
+//     "shubh": false,
+//     "lovish": 67,
+//     "Rohan": undefined
+// }
+// console.log(item["lovish"])
+
+// PRACTICE SET QUESTION CHAPTER 1
+
+
+// // QUESTION 1
+// let  a = "Nagendra "
+// let b = 23
+
+// console.log(a+b)
+
+// //QUESTION 2
+
+// console.log(typeof a)
+
+// //QUESTION 5
+
+// const item = {
+//     "nagend":  95%,
+//     "rahul": 96%,
+//     "amar":54%,
+//     "roy":80%
+//     "rohan": undefined
+// }
+// console.log()
+
+
+// operators in javascript
+
+// console.log("operators in Js")
+// let a = 10;
+// let b = 12;
+
+// console.log("a + b = ", a+b)
+// console.log("a - b = ", a-b)
+// console.log("a * b = ", a*b)
+// console.log("a / b = ", a/b)
+// console.log("a % b = ", a%b)
+
+// console.log("++a = ", ++a)
+// console.log("a++ = ", a++)
+// console.log("--a = ", --a)
+// console.log("a-- = ", a--)
+// console.log("a = ", a) 
+// console.log("a-- = ", a--)
+
+// assignment operators
+
+// let assignment = 1;
+// assignment += 5
+// console.log("a is now = ", a)
+
+
+let a = prompt("Hey whats your age?")
+a = Number.parseInt(a)
+console.log(typeof a)
+
